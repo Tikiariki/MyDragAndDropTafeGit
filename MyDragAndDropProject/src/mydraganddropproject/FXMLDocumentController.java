04d8d5d7c42afda0d4b78c5970072e62b8ddeb30
Tikiariki <68983688+Tikiariki@users.noreply.github.com>
2020-11-20 12:12:39 +1100

    Image Drop Completed

--- MyDragAndDropProject/src/mydraganddropproject/FXMLDocumentController.java
+++ MyDragAndDropProject/src/mydraganddropproject/FXMLDocumentController.java
@@ -1,13 +1,21 @@
 
 package mydraganddropproject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 
 /**
  *
@@ -16,19 +24,26 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    @FXML
-    private Button button;
+    private ImageView imageView;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleDragOver(DragEvent event) {
+        if(event.getDragboard().hasFiles()){
+        event.acceptTransferModes(TransferMode.ANY);
+        }
+    }
+
+    @FXML
+    private void handleDrop(DragEvent event) throws FileNotFoundException {
+        List<File> files = event.getDragboard().getFiles();
+        Image img = new Image(new FileInputStream(files.get(0)));
+        imageView.setImage(img);
+    }
     
 }
